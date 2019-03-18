e0a96cf511acbc8c1717131bb9c8b786e4a3cd02
irfangirindra <46699741+irfangirindra@users.noreply.github.com>
2019-03-18 09:41:20 +0700

    soal4
    Soal nomor 4 UTS FI4278

---
+++ hello.js
@@ -0,0 +1,33 @@
+/* 
+	Hello.js
+	Output hello in text area
+	
+	Irfan Girindra | https://github.com/irfangirindra/UTS
+	
+	Include: <script src="hello.js"></script> in
+	         a HTML file
+	Execute: Refresh web browser viewing the HTML file
+	
+*/
+
+var taOut;
+
+
+main();
+
+function main(){
+	// Set layout of visual elements
+	setElementsLayout();
+}
+
+function setElementsLayout(){
+	//Create text area output
+	taOut = document.createElement("textarea");
+	taOut.style.width = "500px";
+	taOut.style.height = "150px"
+	taOut.style.overflowY = "scroll";
+	taOut.style.float = "left";
+	taOut.value = "Hello, Muhammad Irfan Girindra yang ber-NIM 10215089!";
+	
+	document.body.append(taOut);
+}
